1d18ee22bff88aedf7e71e185b576994e9d79932
TheOnliestMattastic <matthew.poole485@gmail.com>
2026-06-26 16:39:32 -0400

    Created `get_status_grid()` function in Portrait to reduce calls.

--- scripts/entities/ent_Portrait.gd
+++ scripts/entities/ent_Portrait.gd
@@ -1,12 +1,13 @@
 extends TextureRect
 class_name Portrait
 
-@onready var health: Health = %Health
+var status_grid: StatusGrid 
+func get_status_grid() -> StatusGrid:
+	if status_grid == null: status_grid = get_node("%StatusGrid")
+	return status_grid
 
 func add_status_icon(icon: Texture2D, status_name: String) -> void:
-	var status_grid: StatusGrid = %StatusGrid
-	status_grid.add_icon(icon, status_name)
+	get_status_grid().add_icon(icon, status_name)
 
 func remove_status_icon(status_name: String) -> void:
-	var status_grid: StatusGrid = %StatusGrid
-	status_grid.remove_icon(status_name)
+	get_status_grid().remove_icon(status_name)
