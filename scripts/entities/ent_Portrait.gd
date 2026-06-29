935d4930d3c4cd7e5983479ec02b82da584f8b67
TheOnliestMattastic <matthew.poole485@gmail.com>
2026-06-29 02:47:25 -0400

    Further built combat selection menu.

--- scripts/entities/ent_Portrait.gd
+++ scripts/entities/ent_Portrait.gd
@@ -1,11 +1,19 @@
 extends TextureRect
 class_name Portrait
 
+# === Health ===
 var health: Health
 func get_health() -> Health:
 	if health == null: health = get_node("%Health")
 	return health
 
+func set_actor_hp(maximum: int) -> void:
+	get_health().set_hp(maximum)
+
+func set_actor_current_hp(hp: float) -> void:
+	get_health().set_current_hp(hp)
+
+# === Status ===
 var status_grid: StatusGrid 
 func get_status_grid() -> StatusGrid:
 	if status_grid == null: status_grid = get_node("%StatusGrid")
@@ -16,13 +24,3 @@ func add_status_icon(icon: Texture2D, status_name: String) -> void:
 
 func remove_status_icon(status_name: String) -> void:
 	get_status_grid().remove_icon(status_name)
-
-func set_actor_hp(maximum: int) -> void:
-	get_health().set_hp(maximum)
-
-func set_actor_current_hp(hp: float) -> void:
-	get_health().set_current_hp(hp)
-	
-	
-	
-	
